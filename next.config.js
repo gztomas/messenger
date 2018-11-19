9ec09e2b2f9c7c1eedf3ad5cee667d33ca982997
Tomás González <tgonzalez@asapp.com>
2018-11-19 12:16:32 -0300

    fix gh pages deploy

--- next.config.js
+++ next.config.js
@@ -3,4 +3,6 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
   // Fixes npm packages that depend on `fs` module
   webpack: config => ({ ...config, node: { fs: 'empty' } }),
+
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/messenger' : '',
 });
