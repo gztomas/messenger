13664613c737899aef49aaa2f24b6c15495d316a
Tomás González <tgonzalez@asapp.com>
2018-11-23 07:49:58 -0300

    fix standalone page

--- src/initial-state.js
+++ src/initial-state.js
@@ -8,15 +8,15 @@ export const INITIAL_STATE = {
       picture:
         'https://98466-279326-raikfcquaxqncofqfm.stackpathdns.com/admin/assets/images/people/robert_downey_jr._683.jpg',
     },
-    {
-      id: 'laura-contact-id',
-      name: 'Laura',
-      status: 'online',
-      isTyping: false,
-      picture:
-        'https://marriedbiography.com/wp-content/uploads/2017/05/Laura-Harrier-1.jpg',
-    },
   ],
+  me: {
+    id: 'laura-contact-id',
+    name: 'Laura',
+    status: 'online',
+    isTyping: false,
+    picture:
+      'https://marriedbiography.com/wp-content/uploads/2017/05/Laura-Harrier-1.jpg',
+  },
   activeChannel: {
     id: 'laura-rob-channel-id',
     contactId: 'rob-contact-id',
