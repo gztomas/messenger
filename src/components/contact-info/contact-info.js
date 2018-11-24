487297ba0d899493343b6f7233e5f1076e5d8b5b
Tomás González <tgonzalez@asapp.com>
2018-11-24 19:21:01 -0300

    add typing status to contact info

--- src/components/contact-info/contact-info.js
+++ src/components/contact-info/contact-info.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import { Avatar } from 'antd';
 import { StyledDetails, StyledContainer } from './styled';
 
-const ContactInfo = ({ picture, name, status }) => (
+const IS_TYPING_LABEL = 'typing...';
+
+const ContactInfo = ({ picture, name, status, isTyping }) => (
   <StyledContainer>
     <Avatar size="large" src={picture} />
     <StyledDetails>
       <h4>{name}</h4>
-      <span>{status}</span>
+      <span>{isTyping ? IS_TYPING_LABEL : status}</span>
     </StyledDetails>
   </StyledContainer>
 );
@@ -18,6 +20,7 @@ ContactInfo.propTypes = {
   picture: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
+  isTyping: PropTypes.bool.isRequired,
 };
 
 export default ContactInfo;
--- src/components/contact-info/index.js
+++ src/components/contact-info/index.js
@@ -1,7 +1,18 @@
 import { connect } from 'react-redux';
 import ContactInfo from './contact-info';
 
-const mapStateToProps = ({ contacts, channels, activeChannelId, me }) =>
-  contacts[channels[activeChannelId].members.find(id => id !== me.id)];
+const mapStateToProps = ({ contacts, channels, activeChannelId, me }) => {
+  const activeChannel = channels[activeChannelId];
+  const contactId = activeChannel.members.find(id => id !== me.id);
+  const { picture, name, status } = contacts[contactId];
+  const isTyping = activeChannel.typing.some(id => id === contactId);
+
+  return {
+    picture,
+    name,
+    status,
+    isTyping,
+  };
+};
 
 export default connect(mapStateToProps)(ContactInfo);
