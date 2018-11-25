cb2a7697877d801e9d4d9a67bffce56956219d6b
Tomás González <tgonzalez@asapp.com>
2018-11-25 15:22:40 -0300

    fix images path for standalone mode

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = withCSS({
   // Fixes npm packages that depend on `fs` module
   webpack: config => ({ ...config, node: { fs: 'empty' } }),
 
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/messenger' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/messenger/' : '',
 });
